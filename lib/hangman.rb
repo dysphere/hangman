d28a9d2cd1bc3febfad01a5e27b5a36ca7188787
dysphere <keynesiane@gmail.com>
2022-09-23 13:29:27 -0700

    Save and load files

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,13 +1,20 @@
+require "json"
+
 class Hangman
-    def initialize
-        @mistakes = 0
-        @incorrect_letters = Array.new
-        @word_letters = Array.new
-        @empty_array = Array.new
-        @positions = Array.new
-        @word = ""
-        @guess = ""
-        @turn = 0
+    attr_accessor :mistakes, :incorrect_letters, :word_letters,
+    :empty_array, :positions, :word, :guess, :turn
+
+    def initialize(mistakes=0, incorrect_letters=[],
+    word_letters=[], empty_array=[], positions=[],
+    word="", guess="", turn=0)
+        @mistakes = mistakes
+        @incorrect_letters = incorrect_letters
+        @word_letters = word_letters
+        @empty_array = empty_array
+        @positions = positions
+        @word = word
+        @guess = guess
+        @turn = turn
     end
 
     def choose_word
@@ -15,7 +22,7 @@ class Hangman
         file = File.open(fname, "r")
         random_word = file.readlines.sample
         if random_word.length > 4 && random_word.length < 13
-            @word = random_word
+            @word = random_word.chomp
         else
             choose_word
         end
@@ -30,9 +37,29 @@ class Hangman
     end
 
     def play
-        choose_word
-        puts @word
-        empty_word
+        puts "Do you want to load a game? (y/n)"
+        answer = gets.chomp.downcase
+        if answer == "y"
+            puts "Enter the name of your save"
+            save_name = gets.chomp
+            save_file = File.open("#{save_name}.json", "r")
+            save_data = save_file.read
+            load_file = Hangman.from_json(save_data)
+            load_file.continue_play
+        else
+            choose_word
+            empty_word
+            while @mistakes < 7 && @empty_array != @word_letters
+                turn
+                puts "Mistakes: #{@mistakes}/7"
+                puts @empty_array.join(" ")
+                @turn += 1
+            end
+        end
+    end
+
+    def continue_play
+        puts @empty_array.join(" ")
         while @mistakes < 7 && @empty_array != @word_letters
             turn
             puts "Mistakes: #{@mistakes}/7"
@@ -64,16 +91,47 @@ class Hangman
     end
 
     def turn
+        save_game
         puts "Enter a letter"
         @guess = gets.chomp.downcase
         guess_letter
         empty_guess
     end
 
-    def save_game
+    def to_json
+        JSON.dump ({
+            :mistakes => @mistakes,
+            :incorrect_letters => @incorrect_letters,
+            :word_letters => @word_letters,
+            :empty_array => @empty_array,
+            :positions => @positions,
+            :word => @word,
+            :guess => @guess,
+            :turn => @turn
+        })
     end
 
-    def load_game
+    def save_game
+        puts "Do you want to save your game? (y/n)"
+        answer = gets.chomp.downcase
+        if answer == "y"
+            puts "Enter a unique name for your savefile"
+            save_name = gets.chomp
+            save = Hangman.new(@mistakes, @incorrect_letters,
+            @word_letters, @empty_array, @positions, 
+            @word, @guess, @turn)
+            File.open("#{save_name}.json", "w") do |f|
+                f.puts(save.to_json)
+            end
+        end
+    end
+
+    def self.from_json(string)
+        data = JSON.load string
+        new_object = self.new(data['mistakes'], data['incorrect_letters'],
+        data['word_letters'], data['empty_array'], 
+        data['positions'], data['word'], data['guess'],
+        data['turn'])
     end
 end
 
